e0423bda667f2e2e6a4b45e141f2f5f2df151d18
Maz <mazenmagzoub@gmail.com>
2024-09-03 13:19:17 +0100

    add create new post view

---
+++ MobileAcebook/CreatePostView.swift
@@ -0,0 +1,49 @@
+//
+//  CreatePostView.swift
+//  MobileAcebook
+//
+//  Created by Maz on 03/09/2024.
+//
+
+import SwiftUI
+
+struct CreatePostView: View {
+    @State private var userInput: String = ""
+    var body: some View {
+        VStack(alignment: .center){
+            Text("Make a Post").font(.largeTitle).bold()
+            TextField(
+                "Post text, lorem ipsum day...",
+                text: $userInput,
+                axis: .vertical
+            ).textFieldStyle(.roundedBorder)
+                .lineLimit(10, reservesSpace: true)
+                .multilineTextAlignment(.leading)
+                .frame(minWidth: 100, maxWidth: 400, minHeight: 100, maxHeight: 250)
+//                .cornerRadius(40)
+            HStack(alignment: .center, spacing: 3){
+                Button("Add Image"){}
+                    .frame(width: 96, height: 64)
+                    .background(Color(red: 0, green: 0.48, blue: 1))
+                    .cornerRadius(40)
+                    .foregroundColor(.white)
+                
+                Spacer()
+                Button("Create Post"){}
+                    .frame(width: 96, height: 64)
+                    .background(Color(red: 0, green: 0.48, blue: 1))
+                    .cornerRadius(40)
+                    .foregroundColor(.white)
+                
+            }.padding(40)
+            
+        }.frame(maxHeight: 900)
+        .padding()
+            .background(Color(red: 0, green: 0.96, blue: 1))
+        
+    }
+}
+
+#Preview {
+    CreatePostView()
+}
